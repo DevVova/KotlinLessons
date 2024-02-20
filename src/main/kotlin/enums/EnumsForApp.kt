3a6284c403cc2df1097511355f4e958619d052bc
Zabotin Vladimir <ZabotinV78@gmail.com>
2024-02-20 18:22:51 +0600

    Приступил за изучение языка снова и начал с изучения перечислений, создал практически необходимое перечисление дней недели

---
+++ src/main/kotlin/enums/EnumsForApp.kt
@@ -0,0 +1,30 @@
+package enums
+
+fun main() {
+    val dayOfWeek = DaysOfWeek.Tuesday
+    println(dayOfWeek)
+    val dayArray = DaysOfWeek.entries.toTypedArray()//Взяли enum и преобразовали в массив.
+    for (i in dayArray) {
+        print("$i   ")
+    }
+    println()
+
+    //Ниже показан пример как можно создать массив из сокращённых названий дней недели.
+    val daysShortNameArray = dayArray.map { it.shortName }.toTypedArray()
+    for (i in daysShortNameArray) {
+        print("$i   ")
+    }
+    println()
+}
+
+
+//Вот так я думаю нужно создавать дни недели в приложениях.
+enum class DaysOfWeek(val shortName: String) {
+    Monday("Mo"),
+    Tuesday("Tu"),
+    Wednesday("We"),
+    Thursday("Th"),
+    Friday("Fr"),
+    Saturday("Sa"),
+    Sunday("Su")
+}
