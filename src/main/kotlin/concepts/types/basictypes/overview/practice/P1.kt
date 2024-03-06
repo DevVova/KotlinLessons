a82b8f4613f91b3ef54a865013ee284635ea2282
Zabotin Vladimir <ZabotinV78@gmail.com>
2024-03-06 13:47:49 +0500

    Всё сначала, бросил изучать котлин по видео, теперь только по документации

---
+++ src/main/kotlin/concepts/types/basictypes/overview/practice/P1.kt
@@ -0,0 +1,8 @@
+package concepts.types.basictypes.overview.practice
+
+fun main() {
+    val aa = 2
+    println(aa.dec())//Возвратит уменьшенное число 2, dec это метод класса Int(или иначе говоря функция-член).
+    val r = Short.MAX_VALUE
+    println(r)//Возвратится максимальное значение для класса Short. MAX_VALUE- это компаньон объект.
+}
